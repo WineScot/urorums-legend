741a131e8d826b1cbef823c6b110459dfc6b3ad7
Kszesiek <35864414+Kszesiek@users.noreply.github.com>
2019-09-24 22:15:40 +0200

    main menu v0.1

---
+++ Assets/Scripts/MainMenuCanvasController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuCanvasController : MonoBehaviour
+{
+    CanvasGroup bgImage;
+    private float timeSinceCreation = 0f;
+
+    public float timeUntilFadeIn = 0.5f;
+    public int speedOfFadeIn = 20;
+
+    void Start ()
+    {
+        bgImage = GameObject.Find("Canvas").GetComponent<CanvasGroup>();
+        bgImage.alpha = 0f;
+    }
+
+    void FixedUpdate ()
+    {
+        timeSinceCreation += 0.02f;
+        
+        if (bgImage.alpha < 1f && timeSinceCreation >= timeUntilFadeIn)
+        {
+            if (speedOfFadeIn * 0.00390625f + bgImage.alpha <= 1f)
+            {
+                bgImage.alpha += speedOfFadeIn * 0.00390625f;  // 1/256
+                if (bgImage.alpha > 0.9f)
+                    bgImage.interactable = true;
+            }                                           // 1f makes it fully visible, 0f makes it fully transparent.
+            else
+            {
+                bgImage.alpha = 1f;
+                bgImage.interactable = true;
+            }
+        }
+    }
+}
+
